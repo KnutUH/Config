e0555564d6c7ae145d2abda7e7f8bd0ea69f701b
Knut Utne Hollund <knutuh@gmail.com>
2018-05-24 16:35:31 +0200

    ..

--- init.el
+++ init.el
@@ -9,6 +9,7 @@
        '("melpa" . "http://melpa.org/packages/") t)
 
 (package-initialize)
+
 (when (not package-archive-contents)
   (package-refresh-contents))
 
@@ -31,17 +32,24 @@
 
 
 (elpy-enable)
-
+(add-hook 'elpy-mode-hook (lambda () (highlight-indentation-mode -1)))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
  '(custom-enabled-themes (quote (material)))
  '(custom-safe-themes
    (quote
-    ("732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "4a533265a818c5d7b4962afda69fb4b0b3e6af8c17c1f01462e79a82e38c3429" default))))
+    ("732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "4a533265a818c5d7b4962afda69fb4b0b3e6af8c17c1f01462e79a82e38c3429" default)))
+ '(org-mobile-checksum-binary "/d/appl/Git/usr/bin/sha1sum.exe")
+ '(org-mobile-directory "~/org")
+ '(org-mobile-files (quote (org-agenda-files "heime.org" "work.org")))
+ '(package-selected-packages (quote (magit material-theme elpy better-defaults)))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
